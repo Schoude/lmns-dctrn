cbc6b86eece50bcc3ca037acb4a4b86b44ba4df2
Marc <marcbaque1311@gmail.com>
2024-02-16 11:20:20 +0100

    [ ADD ] doctrine installation

--- config/modules.config.php
+++ config/modules.config.php
@@ -6,6 +6,8 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Laminas\Cache',
+    'Laminas\Paginator',
     'Laminas\Mvc\Plugin\FilePrg',
     'Laminas\Mvc\Plugin\FlashMessenger',
     'Laminas\Mvc\Plugin\Identity',
@@ -20,6 +22,10 @@ return [
     'Laminas\Router',
     'Laminas\Validator',
     'Laminas\Diactoros',
+    'Laminas\Cache\Storage\Adapter\Filesystem',
+    'Laminas\Cache\Storage\Adapter\Memory',
+    'DoctrineModule',
+    'DoctrineORMModule',
     'Application',
     'Album',
 ];
