15c377ae82e4dd9411650c0d282d4b554c03619b
Marc <marcbaque1311@gmail.com>
2024-02-21 16:10:29 +0100

    [ ADD ] laminas navigation

--- config/modules.config.php
+++ config/modules.config.php
@@ -6,6 +6,7 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Laminas\Navigation',
     'Laminas\Cache',
     'Laminas\Paginator',
     'Laminas\Mvc\Plugin\FilePrg',
